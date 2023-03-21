b8b99782d5e309aa7503b429396c741f4985a6b2
wendyle <wendyle09@gmail.com>
2023-03-20 19:59:39 -0700

    format news feed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'news-feed';
+  title = 'News Feed';
+  stories = [
+    {
+      id: 8863,
+      time: 1175714200,
+      title: 'My YC app: Dropbox - Throw away your USB drive',
+      url: 'http://www.getdropbox.com/u/2/screencast.html',
+    },
+    {
+      id: 35238878,
+      time: 1679346973,
+      title: 'Why northern Europe is so indebted',
+      url: 'https://theloop.ecpr.eu/why-northern-europe-is-so-indebted/',
+    },
+  ];
 }
---
+++ src/app/shared/interfaces/story.interface.ts
@@ -0,0 +1,6 @@
+export interface Story {
+  id: number;
+  time: number;
+  title: string;
+  url: string;
+}
