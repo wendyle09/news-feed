99b104e3c78935bbecbfc8ea04a3fe6a43d01429
wendyle <wendyle09@gmail.com>
2023-03-23 16:58:09 -0700

    add tests

--- src/app/utils.spec.ts
+++ src/app/utils.spec.ts
@@ -2,8 +2,20 @@ import { formatSearchText } from './utils';
 
 describe('formatSearchText()', () => {
   it('should preverse spaces between double quotes', () => {
-    const formattedText = formatSearchText('"search phrase"');
+    const formattedText = formatSearchText('"over the moon"');
 
-    expect(formattedText).toEqual('search phrase');
+    expect(formattedText).toEqual('over the moon');
+  });
+
+  it('should split and trim words not in double quotes', () => {
+    const formattedText = formatSearchText('over the moon');
+
+    expect(formattedText).toEqual('over+the+moon');
+  });
+
+  it('should handle words inside and outside double quotes', () => {
+    const formattedText = formatSearchText('over "the moon"');
+
+    expect(formattedText).toEqual('over+the moon');
   });
 });
